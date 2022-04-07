a5a52bf8fb73513b78b495d04e962428e92b6736
developer@avokado.co <developer@avokado.co>
2022-04-07 23:12:55 +0530

    first init iOS app

--- diceapp/ViewController.swift
+++ diceapp/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func diceButtonActions(_ sender: Any) {
+        var diceArray=[UIImage(named: "DiceOne.png"), UIImage(named: "DiceTwo.png"), UIImage(named: "DiceThree.png"), UIImage(named: "DiceFour.png"), UIImage(named: "DiceFive.png"), UIImage(named: "DiceSix.png")]
+        
+        diceImageView1.image=diceArray[Int.random(in: 0...5)]
+        diceImageView2.image=diceArray[Int.random(in: 0...5)]
+    }
+   
 
 }
 
